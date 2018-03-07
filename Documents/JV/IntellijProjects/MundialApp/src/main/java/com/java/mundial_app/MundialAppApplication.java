fc22c045cf174a42c361c1db65b1c34113d945a5
Juan Villanueva Aguinaga <jvavillanuevaaguinaga@gmail.com>
2018-03-07 15:51:45 -0500

    Primer Commit para el Mundial App

---
+++ Documents/JV/IntellijProjects/MundialApp/src/main/java/com/java/mundial_app/MundialAppApplication.java
@@ -0,0 +1,12 @@
+package com.java.mundial_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundialAppApplication.class, args);
+	}
+}
